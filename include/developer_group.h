54a35b0e9a3c57061702bb86fa3553e1f6755b42
ihlermario <122440141+ihlermario@users.noreply.github.com>
2023-03-16 12:01:56 +0000

    create grouplogo

--- include/developer_group.h
+++ include/developer_group.h
@@ -1,8 +1,12 @@
 #ifndef DEVELOPER_GROUP_Hr789sn
 #define DEVELOPER_GROUP_Hr789sn
+#define LI 38
+#define CO 181
 typedef struct
 {
 } developer_group;
 
 void developer_group_init();
+
+char grouplogo[LI][CO];
 #endif
